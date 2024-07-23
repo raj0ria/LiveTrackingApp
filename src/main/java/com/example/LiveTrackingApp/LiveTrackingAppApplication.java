3871b7deb71933e14c8d7150f0d664f0cbacca39
AshishSharma-Pepcus <ashish.sharma@pepcus.com>
2024-07-23 21:44:48 +0530

    Initial commit

---
+++ src/main/java/com/example/LiveTrackingApp/LiveTrackingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.LiveTrackingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveTrackingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveTrackingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LiveTrackingApp/LiveTrackingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LiveTrackingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveTrackingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
